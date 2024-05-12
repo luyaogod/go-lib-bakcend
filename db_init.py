84b2f158f11e27238fe966217cbf61d38204c75a
mariokkm <maluyao0310@gmail.com>
2024-05-12 22:34:32 +0800

    更新用户表

--- db_init.py
+++ db_init.py
@@ -102,17 +102,17 @@ async def main():
         {
             "username": "王姿童",
             "uuid": "1a7e7a53-e204-4dd7-9eb0-b8a7c373a161",
-            "balance": 26
+            "balance": 24
         },
         {
             "username": "客户3",
             "uuid": "4d69cb47-86bc-4473-ab26-44ebe699c1e8",
-            "balance": 0
+            "balance": 27
         },
         {
             "username": "客户2",
             "uuid": "0ca24344-d424-4fe1-95d1-4a22fb6d5e3a",
-            "balance": 26
+            "balance": 24
         },
         {
             "username": "客户4",
@@ -122,7 +122,7 @@ async def main():
         {
             "username": "客户5",
             "uuid": "7c018e97-1a82-475e-8b20-77d37a7c512d",
-            "balance": 20
+            "balance": 18
         },
         {
             "username": "客户6",
@@ -147,7 +147,7 @@ async def main():
         {
             "username": "施韵",
             "uuid": "4862a048-a34f-4eb8-802c-979f3d1c38bf",
-            "balance": 23
+            "balance": 21
         },
         {
             "username": "客户10",
@@ -172,22 +172,22 @@ async def main():
         {
             "username": "客户14",
             "uuid": "5f0a3545-8f81-4511-b0ac-a91bd39418a7",
-            "balance": 19
+            "balance": 18
         },
         {
             "username": "客户15",
             "uuid": "60874674-2193-4e08-b084-1abbc131d7f6",
-            "balance": 25
+            "balance": 22
         },
         {
             "username": "客户16",
             "uuid": "76b350f8-1437-4be3-89e1-0d8441145534",
-            "balance": 22
+            "balance": 20
         },
         {
             "username": "客户17",
             "uuid": "1dddbccb-45d7-448f-9a47-83dc1be3c2bd",
-            "balance": 20
+            "balance": 18
         },
         {
             "username": "客户18",
@@ -197,7 +197,7 @@ async def main():
         {
             "username": "客户19",
             "uuid": "afeb3c89-ea12-420c-b54a-8503ecd0df8e",
-            "balance": 18
+            "balance": 16
         },
         {
             "username": "客户20",
@@ -217,22 +217,22 @@ async def main():
         {
             "username": "客户23",
             "uuid": "b6181614-f9ac-4806-b11f-88c3b630252e",
-            "balance": 67
+            "balance": 64
         },
         {
             "username": "客户24",
             "uuid": "3ccb8595-d68a-41be-9594-8b76b8a3f30a",
-            "balance": 25
+            "balance": 23
         },
         {
             "username": "客户25",
             "uuid": "a1d5f0cc-e9d4-46c8-b3ec-0cbd9484f856",
-            "balance": 27
+            "balance": 25
         },
         {
             "username": "客户26",
             "uuid": "4ae39ee3-2fb6-4454-8284-248de031e08d",
-            "balance": 55
+            "balance": 54
         },
         {
             "username": "客户27",
@@ -244,6 +244,11 @@ async def main():
             "uuid": "8199c088-3a28-48e5-b16c-0528c2fcec98",
             "balance": 0
         },
+        {
+            "username": "杜翰林",
+            "uuid": "008f5727-0e88-4e3d-8a25-448474f1e392",
+            "balance": 9998
+        }
     ]
     for i in data:
         await User.create(username=i['username'], uuid=i['uuid'],balance=i['balance'])
